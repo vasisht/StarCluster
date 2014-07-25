6b8950f92046e0f22fc39059a4a5a5977382cb9e
Vasisht Tadigotla <vasisht.tadigotla@courtagen.com>
2014-07-25 13:33:45 -0400

    use rpcbind instead of portmap for ubuntu13.10

--- starcluster/node.py
+++ starcluster/node.py
@@ -688,7 +688,7 @@ class Node(object):
 
     def start_nfs_server(self):
         log.info("Starting NFS server on %s" % self.alias)
-        self.ssh.execute('/etc/init.d/portmap start')
+        self.ssh.execute('service rpcbind restart')
         self.ssh.execute('mount -t rpc_pipefs sunrpc /var/lib/nfs/rpc_pipefs/',
                          ignore_exit_status=True)
         #self.ssh.execute('/etc/init.d/nfs start')
@@ -702,7 +702,7 @@ class Node(object):
         server_node - remote server node that is sharing the remote_paths
         remote_paths - list of remote paths to mount from server_node
         """
-        self.ssh.execute('/etc/init.d/portmap start')
+        self.ssh.execute('service rpcbind restart')
         # TODO: move this fix for xterm somewhere else
         self.ssh.execute('mount -t devpts none /dev/pts',
                          ignore_exit_status=True)
