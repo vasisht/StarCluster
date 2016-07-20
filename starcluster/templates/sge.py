472c89bb513aa2660c248ee45fcb18c9f8575e31
Vasisht Tadigotla <vtadigotla@exosomedx.com>
2016-07-20 16:37:33 -0400

    Change allocation to pe_slots

--- starcluster/templates/sge.py
+++ starcluster/templates/sge.py
@@ -66,7 +66,7 @@ user_lists        NONE
 xuser_lists       NONE
 start_proc_args   /bin/true
 stop_proc_args    /bin/true
-allocation_rule   $fill_up
+allocation_rule   $pe_slots
 control_slaves    TRUE
 job_is_first_task FALSE
 urgency_slots     min
